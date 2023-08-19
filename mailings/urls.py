788869ff9721445e3d5d51c9d59bfdb86c36d0f3
Dmitry <dima.captan@yandex.ru>
2023-08-19 18:14:02 +0300

    added a new path for change_status view

--- mailings/urls.py
+++ mailings/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from mailings.apps import MailingsConfig
 from mailings.views import (
     IndexView, MailingListView, MailingDetailView, MailingCreateView,
-    MailingUpdateView, MailingDeleteView
+    MailingUpdateView, MailingDeleteView, change_status
 )
 
 app_name = MailingsConfig.name
@@ -14,5 +14,6 @@ urlpatterns = [
     path('mailings/<slug:slug>/', MailingDetailView.as_view(), name='mailing_detail'),
     path('create/', MailingCreateView.as_view(), name='mailing_create'),
     path('update/<slug:slug>/', MailingUpdateView.as_view(), name='mailing_update'),
-    path('delete/<slug:slug>/', MailingDeleteView.as_view(), name='mailing_delete')
+    path('delete/<slug:slug>/', MailingDeleteView.as_view(), name='mailing_delete'),
+    path('status_change/<slug:slug>/', change_status, name='mailing_status')
 ]
