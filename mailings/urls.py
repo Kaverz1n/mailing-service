93d8f10eac84da93ec99ed0414bc36638d487f0a
Dmitry <dima.captan@yandex.ru>
2023-08-20 13:09:39 +0300

    added new paths for client-views

--- mailings/urls.py
+++ mailings/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from mailings.apps import MailingsConfig
 from mailings.views import (
     IndexView, MailingListView, MailingDetailView, MailingCreateView,
-    MailingUpdateView, MailingDeleteView, change_status
+    MailingUpdateView, MailingDeleteView, change_status, ClientListView,
+    ClientDetailView, ClientCreateView, ClientUpdateView, ClientDeleteView
 )
 
 app_name = MailingsConfig.name
@@ -12,8 +13,13 @@ urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('mailings/', MailingListView.as_view(), name='mailing_list'),
     path('mailings/<slug:slug>/', MailingDetailView.as_view(), name='mailing_detail'),
-    path('create/', MailingCreateView.as_view(), name='mailing_create'),
-    path('update/<slug:slug>/', MailingUpdateView.as_view(), name='mailing_update'),
-    path('delete/<slug:slug>/', MailingDeleteView.as_view(), name='mailing_delete'),
-    path('status_change/<slug:slug>/', change_status, name='mailing_status')
+    path('mailing_create/', MailingCreateView.as_view(), name='mailing_create'),
+    path('mailing_update/<slug:slug>/', MailingUpdateView.as_view(), name='mailing_update'),
+    path('mailing_delete/<slug:slug>/', MailingDeleteView.as_view(), name='mailing_delete'),
+    path('status_change/<slug:slug>/', change_status, name='mailing_status'),
+    path('clients/', ClientListView.as_view(), name='client_list'),
+    path('clients/<int:pk>/', ClientDetailView.as_view(), name='client_detail'),
+    path('client_create/', ClientCreateView.as_view(), name='client_create'),
+    path('client_update/<int:pk>/', ClientUpdateView.as_view(), name='client_update'),
+    path('client_delete/<int:pk>/', ClientDeleteView.as_view(), name='client_delete')
 ]
