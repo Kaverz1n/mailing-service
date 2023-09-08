703bf2004916f6f20341ee77ed77c7811941cd44
Dmitry <dima.captan@yandex.ru>
2023-09-08 17:05:02 +0300

    added ResetPasswordForm

--- users/forms.py
+++ users/forms.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.contrib.auth.forms import UserCreationForm
 
 from users.models import User
@@ -11,3 +12,21 @@ class RegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('email', 'password1', 'password2',)
+
+
+class ResetPasswordForm(forms.Form):
+    '''
+    Форма сброса пароля
+    '''
+    email = forms.EmailField(max_length=254, label='e-mail')
+
+    def clean_email(self) -> str:
+        cleaned_data = self.cleaned_data['email']
+        try:
+            user = User.objects.get(email=cleaned_data)
+            if not user.is_active:
+                raise forms.ValidationError('Пользователь с такими данными не существует или не подтвержден!')
+        except User.DoesNotExist:
+            raise forms.ValidationError('Пользователь с такими данными не существует или не подтвержден!')
+
+        return cleaned_data
