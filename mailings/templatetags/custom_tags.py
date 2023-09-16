213f908f45d0b168d5cf9b9511af299f0dc41991
Dmitry <dima.captan@yandex.ru>
2023-09-16 17:06:37 +0300

    added caching in filter has_group

--- mailings/templatetags/custom_tags.py
+++ mailings/templatetags/custom_tags.py
@@ -1,4 +1,6 @@
 from django import template
+from django.conf import settings
+from django.core.cache import cache
 
 register = template.Library()
 
@@ -24,4 +26,13 @@ def has_group(user, group_name) -> bool:
     :param group_name: имя группы
     :return: bool-значение
     '''
+    if settings.CACHE_ENABLED:
+        cache_key = f'user_belongs_{user.pk}_{group_name}'
+        users_group = cache.get(cache_key)
+        if users_group is None:
+            users_group = user.groups.filter(name=group_name).exists()
+            cache.set(cache_key, users_group, 3600)
+
+        return users_group
+
     return user.groups.filter(name=group_name).exists()
