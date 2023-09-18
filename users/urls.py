9c7a270b5b4b08dda7b068ce54a0acf2fc25800e
Dmitry <dima.captan@yandex.ru>
2023-09-18 18:20:48 +0300

    added cache_page in sent_confirm_emai confirmed_email, fail_confirm_email and password_sent_email paths

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.views import LogoutView
 from django.urls import path
+from django.views.decorators.cache import cache_page
 
 from users.apps import UsersConfig
 from users.views import (
@@ -16,11 +17,11 @@ urlpatterns = [
     path('register/', UserRegisterView.as_view(), name='register'),
     path('login/', UserLoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
-    path('sent_confirm_email/', UserSentConfirmEmail.as_view(), name='email_sent_confirm'),
+    path('sent_confirm_email/', cache_page(3600)(UserSentConfirmEmail.as_view()), name='email_sent_confirm'),
     path('confirm_email/<str:uidb64>/<str:token>/', UserConfirmEmailView.as_view(), name='email_confirm'),
-    path('confirmed_email/', UserConfirmedEmail.as_view(), name='email_confirmed'),
-    path('fail_confirm_email/', UserFailConfirmEmail.as_view(), name='email_fail_confirm'),
+    path('confirmed_email/', cache_page(3600)(UserConfirmedEmail.as_view()), name='email_confirmed'),
+    path('fail_confirm_email/', cache_page(3600)(UserFailConfirmEmail.as_view()), name='email_fail_confirm'),
     path('password_reset/', UserPasswordResetView.as_view(), name='password_reset'),
-    path('password_sent_email/', UserSentPassword.as_view(), name='password_sent_email'),
+    path('password_sent_email/', cache_page(3600)(UserSentPassword.as_view()), name='password_sent_email'),
     path('change_active/<int:pk>/', UserChangeActive.as_view(), name='user_change_active')
 ]
