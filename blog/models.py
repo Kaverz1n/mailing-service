21bc64f0359b97d4705d7df873cb593d2e4d141d
Dmitry <dima.captan@yandex.ru>
2023-09-21 16:46:46 +0300

    libraries are arranged alphabetically and added type hints

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,8 @@
-from PIL import Image
 from django.db import models
 
 from mailings.models import NULLABLE
+
+from PIL import Image
 
 
 class Article(models.Model):
@@ -14,15 +15,16 @@ class Article(models.Model):
     view_count = models.PositiveIntegerField(verbose_name='Кол-во просмотров', default=0, **NULLABLE)
     publish_date = models.DateTimeField(auto_now_add=True, auto_now=False, verbose_name='Дата публикации', **NULLABLE)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f'{self.title}'
 
-    def save(self, *args, **kwargs):
+    def save(self, *args, **kwargs) -> None:
         super().save(*args, **kwargs)
 
         img = Image.open(self.image.path)
         max_size = (1200, 800)
 
+        # сжимает изображения до 1200x800
         if img.width != max_size[0] or img.height != max_size[1]:
             resized_image = img.resize(max_size)
             resized_image.save(self.image.path)
