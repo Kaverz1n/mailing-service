d4ae64513a5f7872f3919b5a0029dc7a79f04b82
Dmitry <dima.captan@yandex.ru>
2023-09-21 16:39:16 +0300

    print changed to self.stderr.write

--- mailings/management/commands/fill.py
+++ mailings/management/commands/fill.py
@@ -5,8 +5,9 @@ class Command(BaseCommand):
     '''
     Комманда для заполнения данными базу данных
     '''
-    def handle(self, *args, **options):
+
+    def handle(self, *args, **options) -> None:
         try:
             call_command('loaddata', 'database_data.json')
         except:
-            print('Ошибка загрузки данных!')
+            self.stderr.write('Ошибка загрузки данных!')
